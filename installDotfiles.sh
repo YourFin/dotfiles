60a8c7a214740c2fda42420a37e8bfa1ca731698
Patrick Nuckolls <nuckolp@amazon.com>
2019-06-03 09:02:41 -0700

    loops through config dir instead of linking directly

--- installDotfiles.sh
+++ installDotfiles.sh
@@ -2,6 +2,8 @@
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
+# First argument: file in this directory
+# Second argument: destination relative to $HOME
 linkFile ()
 {
 	first="$SCRIPTPATH/$1"
@@ -12,17 +14,20 @@ linkFile ()
 		read userInput
 		if ! [ "$userInput" = "n" ]
 		then
-			ln -sf $first $second 
+      rm -rf $second
+			ln -sf $first $second
 		fi
 	else
-		ln -sf $first $second 
+		ln -sf $first $second
 	fi
 }
 
-if [ -e "~/.config" ] ; then
-    cp -rn ~/.config/* $SCRIPTPATH/config
-fi
-linkFile config .config
+#if [ -e "~/.config" ] ; then
+#    cp -rn ~/.config/* $SCRIPTPATH/config
+#fi
+for file in $(ls $SCRIPTPATH/config); do
+    linkFile config/$file .config/$file
+done
 
 for file in $(ls -a | grep -e '^\.[a-zA-Z0-9]' | grep -v git) ; do
     linkFile $file $file
