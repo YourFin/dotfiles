245f989b89c3d8ab8fd40ad04c8a8fcd6c4c6e2b
YourFin <d.junkpobox+git@gmail.com>
2020-08-14 17:02:28 -0700

    add macos kitty config

--- config/nixpkgs/program-cfg/kitty.nix
+++ config/nixpkgs/program-cfg/kitty.nix
@@ -4,22 +4,27 @@
   programs.kitty = {
     enable = true;
     keybindings = {
-      "ctrl+shift+v" =        "paste_from_clipboard";
-      "ctrl+shift+s" =        "paste_from_selection";
-      "ctrl+shift+c" =        "copy_to_clipboard";
-      "shift+insert" =        "paste_from_selection ";
-      "ctrl+shift+t" =        "new_tab_with_cwd";
-      "ctrl+shift+x" =        "close_tab";
-      "ctrl+shift+h" =        "previous_tab";
-      "ctrl+shift+l" =        "next_tab";
-      "ctrl+shift+left" =     "move_tab_forward";
-      "ctrl+shift+right" =    "move_tab_backward";
-      "ctrl+shift+alt+t" =    "set_tab_title";
-      "ctrl+shift+equal" =    "increase_font_size";
-      "ctrl+shift+minus" =    "decrease_font_size";
+      "ctrl+shift+v" = "paste_from_clipboard";
+      "ctrl+shift+s" = "paste_from_selection";
+      "ctrl+shift+c" = "copy_to_clipboard";
+      "shift+insert" = "paste_from_selection ";
+      "ctrl+shift+t" = "new_tab_with_cwd";
+      "ctrl+shift+x" = "close_tab";
+      "ctrl+shift+h" = "previous_tab";
+      "ctrl+shift+l" = "next_tab";
+      "ctrl+shift+left" = "move_tab_forward";
+      "ctrl+shift+right" = "move_tab_backward";
+      "ctrl+shift+alt+t" = "set_tab_title";
+      "ctrl+shift+equal" = "increase_font_size";
+      "ctrl+shift+minus" = "decrease_font_size";
       "ctrl+shift+backspace" = "restore_font_size";
-      "ctrl+shift+u" =        "input_unicode_character";
+      "ctrl+shift+u" = "input_unicode_character";
     };
+
+    extraConfig = ''
+      macos_option_as_alt yes
+      macos_quit_when_last_window_closed yes
+    '';
     settings = {
       allow_remote_control = "no";
       enable_audio_bell = false;
