3094057a2c3ddce3bc7f239ab5813baf4becd806
YourFin <d.junkpobox+git@gmail.com>
2021-09-22 13:37:18 -0700

    Clean up magit evil keybinds

--- share/dot/config/doom/langs/git.el
+++ share/dot/config/doom/langs/git.el
@@ -1,6 +1,9 @@
 ;;; ~/g/dotfiles/config/doom/langs/git.el -*- lexical-binding: t; -*-
 
 ;; Who the fuck uses macros in git commits
-(evil-define-minor-mode-key 'normal 'git-commit-mode
-  "q" 'with-editor-finish
-  "Q" 'with-editor-cancel)
+
+(after! (magit evil)
+  (evil-define-minor-mode-key 'normal 'git-commit-mode
+    "q" 'with-editor-finish
+    "Q" 'with-editor-cancel)
+  )
