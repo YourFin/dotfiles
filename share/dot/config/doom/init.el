02b707c472a63d9da89a61e4fc9fd2e8a16c02cf
YourFin <d.junkpobox+git@gmail.com>
2021-11-17 12:38:45 -0800

    [doom] Enable lsp

--- share/dot/config/doom/init.el
+++ share/dot/config/doom/init.el
@@ -109,6 +109,7 @@
        ;;terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
        upload            ; map local to remote projects via ssh/ftp
+       lsp
 
        :lang
        ;;agda              ; types of types of types of types...
