6390f731aec0f7053dcf5936ed07372e5bfab965
YourFin <d.junkpobox+git@gmail.com>
2022-12-02 01:33:11 -0800

    Drop fill-column, ligatures

--- program-cfg/doom/init.el
+++ program-cfg/doom/init.el
@@ -30,9 +30,8 @@
        doom-dashboard    ; a nifty splash screen for Emacs
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
        (emoji +unicode)  ; emoji in emacs
-       ;fill-column       ; a `fill-column' indicator
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
-       ligatures
+       ;; ligatures
        hydra
        indent-guides     ; highlighted indent columns
        modeline          ; snazzy, Atom-inspired modeline, plus API
