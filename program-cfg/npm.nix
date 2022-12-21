d4396a6bc5db6e9d87fa83cd8c6b15f56bd1bd6a
YourFin <d.junkpobox+git@gmail.com>
2022-12-20 16:16:17 -0800

    Configure npm logs dir to cache, refactor npm cfg

--- program-cfg/npm.nix
+++ program-cfg/npm.nix
@@ -1,9 +1,26 @@
 { config, lib, pkgs, ... }:
 
-{
+let
+  dirs = rec {
+    npm = "${config.xdg.cacheHome}/npm";
+    prefix = "${npm}/global/";
+    logs = "${npm}/logs/";
+    store = "${npm}/store/";
+  };
+in {
+  home.activation.initNpmFolders = lib.hm.dag.entryAfter [ "writeBoundary" ] (''
+    if [ ''${VERBOSE_ARG+x} ]; then
+      VERBOSE_ARG="-v"
+    fi
+
+  '' + (pkgs.lib.concatMapStringsSep "\n" (folder: ''
+    $DRY_RUN_CMD mkdir -p $VERBOSE_ARG ${folder}
+  '') (pkgs.lib.attrValues dirs)));
+
   home.file.".npmrc".text = ''
-    prefix=${config.home.homeDirectory}/.local/share/npm/global/
-    store=${config.home.homeDirectory}/.local/share/npm/npm-store
+    prefix=${dirs.prefix}
+    store=${dirs.store}
+    logs-dir=${dirs.logs}
     shell=zsh
   '';
 }
