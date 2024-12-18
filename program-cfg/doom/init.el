e7e88f118a36ebfc46e0e9268d96707278a87f5d
YourFin <d.junkpobox+git@gmail.com>
2024-12-17 22:03:15 -0800

    feat(doom): haskell tree-sitter

--- program-cfg/doom/init.el
+++ program-cfg/doom/init.el
@@ -128,7 +128,7 @@
        ;;faust             ; dsp, but you get to keep your soul
        ;;fsharp           ; ML stands for Microsoft's Language
        go                ; the hipster dialect
-       (haskell +lsp)  ; a language that's lazier than I am
+       (haskell +lsp +tree-sitter)  ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ;
        (java +meghanada) ; the poster child for carpal tunnel syndrome
