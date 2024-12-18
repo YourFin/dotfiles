902c56398ae178aa466e59a98465342e0130162a
YourFin <d.junkpobox+git@gmail.com>
2024-12-17 22:04:44 -0800

    feat(doom): wire in haskell foramtter

--- program-cfg/doom/langs/haskell.el
+++ program-cfg/doom/langs/haskell.el
@@ -1,3 +1,6 @@
 ;;; ~/g/dotfiles/config/doom/langs/haskell.el -*- lexical-binding: t; -*-
 
 (map! :mode 'haskell-error-mode "q" #'evil-delete-window)
+
+(after! lsp-haskell
+  (setq lsp-haskell-formatting-provider "stylish-haskell"))
