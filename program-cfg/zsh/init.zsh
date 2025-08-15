38fe28399e55d9333ae00088b9f9931f4eff285b
YourFin <src@patrickn.co>
2025-08-15 00:46:06 -0700

    zsh: remove idea of "compinit"

--- program-cfg/zsh/init.zsh
+++ program-cfg/zsh/init.zsh
@@ -1,5 +1,5 @@
 ################################
-# Begin before-compinit.sh
+# Begin init.zsh
 ################################
 # Allows tramp to work by setting the terminal to be as boring as possible
 # Requires (setq tramp-terminal-type "tramp") somewhere in the emacs
@@ -23,17 +23,7 @@ then
   exec $ZSH -$- "$@"
 fi
 
-# Kitty integration
-if test -n "$KITTY_INSTALLATION_DIR"; then
-    export KITTY_SHELL_INTEGRATION="enabled"
-    autoload -Uz -- "$KITTY_INSTALLATION_DIR"/shell-integration/zsh/kitty-integration
-    kitty-integration
-    unfunction kitty-integration
-fi
-
 (( ${+commands[direnv]} )) && emulate zsh -c "$(direnv export zsh)"
-
-typeset -g POWERLEVEL9K_CONFIG_FILE=/dev/null
 
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
@@ -60,6 +50,41 @@ if [[ -t 0 && $- = *i* ]]
 then
     stty -ixon
 fi
-################################
-# End before-compinit.sh
-################################
+
+setopt BANG_HIST # Treat the '!' character specially during expansion.
+unsetopt beep
+setopt HIST_IGNORE_ALL_DUPS # Delete old recorded entry if new entry is a duplicate.
+setopt HIST_FIND_NO_DUPS    # Do not display a line previously found.
+setopt HIST_REDUCE_BLANKS   # Remove superfluous blanks before recording entry.
+setopt HIST_VERIFY          # Don't execute immediately upon history expansion.
+setopt appendhistory extendedglob
+
+## Draw a line between commands
+# Check if \u2501 is printable
+if [ -z "$({ echo -n "\u2501" >/dev/null; } 2>&1)" ]; then
+    export DRAW_LINE_CHAR="\u2501"
+else
+    export DRAW_LINE_CHAR="-"
+fi
+MAX_SEPERATOR_WIDTH=60
+draw_line() {
+    print -Pn "%B%F{249}"
+    # https://superuser.com/a/86342
+    printf "$DRAW_LINE_CHAR"'%.0s' {1..$((COLUMNS < MAX_SEPERATOR_WIDTH ? COLUMNS : MAX_SEPERATOR_WIDTH))}
+    print -Pn "%F{reset}%b\n"
+}
+
+typeset -a preexec_functions
+
+preexec_functions+=("draw_line")
+
+bindkey '^r' history-incremental-pattern-search-backward
+
+# Direnv
+
+if which "direnv" &>/dev/null; then
+    eval "$(direnv hook zsh)"
+fi
+
+# Ocaml
+test -r "$HOME"/.local/usr/opam/opam-init/init.zsh && . "$HOME"/.local/usr/opam/opam-init/init.zsh >/dev/null 2>/dev/null || true
